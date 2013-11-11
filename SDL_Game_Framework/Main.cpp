2f2e35cf81dd9b54d89aa2d2da828f16dc63020b
Vorskabobble <coreybradford1@googlemail.com>
2013-11-11 19:15:58 +0000

    Update Main.cpp

--- SDL_Game_Framework/Main.cpp
+++ SDL_Game_Framework/Main.cpp
@@ -2,7 +2,6 @@
 
 int main(int argc, char* argv[]){
         GameBase::initSDL(1280, 720);
-		cout << "SDL init" << endl;
         ScreenManager* myManager = new ScreenManager();
 
         delete myManager;
